acd1aa8973b75af255858811c279f4cb3526152c
yunhayeon <yhy5049@naver.com>
2025-03-06 15:08:27 +0900

    chore: setup Jest

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jsdom",
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+
+  transformIgnorePatterns: ["/node_modules/"],
+
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/__test__/**/*.[jt]s?(x)"],
+
+  clearMocks: true,
+};
+
+export default createJestConfig(config);
