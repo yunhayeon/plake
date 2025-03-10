0f6057180c4c21c7c72e0a308ff5d5f4ffedf063
yunhayeon <yhy5049@naver.com>
2025-03-10 09:20:19 +0900

    chore: extend Tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,36 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        purple: {
+          50: "#FBF4FF",
+          100: "#F6EAFF",
+          200: "#F2E0FF",
+          300: "#DCA9FF",
+          400: "#BE60FF",
+          500: "#A320FF",
+          600: "#9600FF",
+          700: "#8400E0",
+          800: "#7201C0",
+          900: "#5F00A1",
+          950: "#54048C",
+        },
+        gray: {
+          50: "#F9FAFB",
+          100: "#F3F4F6",
+          200: "#E5E7EB",
+          300: "#D1D5DB",
+          400: "#9CA3AF",
+          500: "#6B7280",
+          600: "#4B5563",
+          700: "#374151",
+          800: "#1F2937",
+          900: "#111827",
+          950: "#030712",
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
